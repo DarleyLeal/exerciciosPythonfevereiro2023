f14bd2fef92fcc8029e3e30717c09ad4897684aa
Darley Leal <108217516+DarleyLeal@users.noreply.github.com>
2023-02-04 18:35:47 -0300

    Update moeda.py

--- moeda.py
+++ moeda.py
@@ -37,4 +37,13 @@ def metade(num, formato=False):
 def real(preco=0, dinheiro='R$'):
     return f'{dinheiro}{preco:.2f}'.replace('.', ',')
 
-
+def resumo(preco=0, taxa=0, taxa1=0):
+    print('=' * 40)
+    print(f'\033[1;33mRESUMO DE VALORES\033[m'.center(40))
+    print('=' * 40)
+    print(f'Preço analisado: \t{real(preco)}')
+    print(f'Dobro do preço:  \t{dobro(preco, True)}')
+    print(f'Metade do preço: \t{metade(preco), True}')
+    print(f'{taxa}% de aumento: \t\{aumento(preco, taxa, True)}')
+    print(f'{taxa1}% de redução: \t{diminuir(preco, taxa1, True)}')
+    print('=' * 40)
